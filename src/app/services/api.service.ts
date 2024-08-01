4eacdc0594a93f4990415178e56b3d8f23c1158e
Zosoled <zosoled@codecow.xyz>
2024-08-01 01:35:39 -0700

    Remove redundant import line.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {HttpHeaders} from '@angular/common/http';
-import {NodeService} from './node.service';
-import {AppSettingsService} from './app-settings.service';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { NodeService } from './node.service';
+import { AppSettingsService } from './app-settings.service';
 import { TxType } from './util.service';
 
 @Injectable()
