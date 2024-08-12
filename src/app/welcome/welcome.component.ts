642be5b6399dc380b81c89eddddfbd7e616dca8c
Zosoled <zosoled@codecow.xyz>
2024-08-11 23:18:50 -0700

    Reorganize imports.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from '../../environments/environment';
-import { WalletService } from '../services/wallet.service';
 import { AppSettingsService } from '../services/app-settings.service';
+import { WalletService } from '../services/wallet.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-welcome',
