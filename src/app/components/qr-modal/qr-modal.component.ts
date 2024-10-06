a386958b5c63ee2c2468ab7090e6754d2ae7a682
Zosoled <zosoled@codecow.xyz>
2024-10-06 01:58:46 -0700

    Replace bip39 dependency with libnemo in QR scanning modal component.

--- src/app/components/qr-modal/qr-modal.component.ts
+++ src/app/components/qr-modal/qr-modal.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
-import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-import { NotificationService } from '../../services/notification.service';
-import { BarcodeFormat } from '@zxing/library';
-import { BehaviorSubject } from 'rxjs';
-import { UtilService } from '../../services/util.service';
-import * as bip39 from 'bip39';
+import { Component, Input, OnInit } from '@angular/core'
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
+import { NotificationService } from '../../services/notification.service'
+import { BarcodeFormat } from '@zxing/library'
+import { BehaviorSubject } from 'rxjs'
+import { UtilService } from '../../services/util.service'
+import { Blake2bWallet } from 'libnemo'
 
-export type QRType = 'account' | 'hash' | 'mnemonic' | 'generic';
+export type QRType = 'account' | 'hash' | 'mnemonic' | 'generic'
 
 @Component({
   selector: 'app-qr-modal',
@@ -16,9 +16,9 @@ export type QRType = 'account' | 'hash' | 'mnemonic' | 'generic';
 export class QrModalComponent implements OnInit {
 
   @Input() title = 'QR Scanner';
-  @Input() reference: string;
-  @Input() type: QRType;
-  availableDevices: MediaDeviceInfo[];
+  @Input() reference: string
+  @Input() type: QRType
+  availableDevices: MediaDeviceInfo[]
   currentDevice: MediaDeviceInfo = null;
   nano_scheme = /^(xrb|nano|nanorep|nanoseed|nanokey):.+$/g;
 
@@ -29,89 +29,92 @@ export class QrModalComponent implements OnInit {
     BarcodeFormat.QR_CODE,
   ];
 
-  hasDevices: boolean;
-  hasPermission: boolean;
+  hasDevices: boolean
+  hasPermission: boolean
 
   torchEnabled = false;
   torchAvailable$ = new BehaviorSubject<boolean>(false);
   tryHarder = false;
 
-  constructor(
+  constructor (
     public activeModal: NgbActiveModal,
     private notifcationService: NotificationService,
     private util: UtilService,
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit (): void {
   }
 
-  onCamerasFound(devices: MediaDeviceInfo[]): void {
-    this.availableDevices = devices;
-    this.hasDevices = Boolean(devices && devices.length);
+  onCamerasFound (devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices
+    this.hasDevices = Boolean(devices && devices.length)
   }
 
-  onCodeResult(resultString: string) {
-    let type: QRType = null;
-    let content = '';
+  async onCodeResult (resultString: string) {
+    let type: QRType = null
+    let content = ''
     // account
     if (this.util.account.isValidAccount(resultString)) {
-      type = 'account';
-      content = resultString;
-    } else if (bip39.validateMnemonic(resultString)) {
-      type = 'mnemonic';
-      content = resultString;
+      type = 'account'
+      content = resultString
+    } else if (/ /.test(resultString)) {
+      try {
+        await Blake2bWallet.fromMnemonic('tmp', resultString)
+        type = 'mnemonic'
+        content = resultString
+      } catch (err) { }
     } else if (resultString.length === 128) {
       // includes deterministic R value material which we ignore
-      resultString = resultString.substring(0, 64);
+      resultString = resultString.substring(0, 64)
       if (this.util.nano.isValidHash(resultString)) {
-        type = 'hash';
-        content = resultString;
+        type = 'hash'
+        content = resultString
       }
     } else if (this.util.nano.isValidHash(resultString)) {
-      type = 'hash';
-      content = resultString;
+      type = 'hash'
+      content = resultString
     } else if (this.nano_scheme.test(resultString)) {
       // This is a valid Nano scheme URI
-      const url = new URL(resultString);
-      content = url.pathname;
+      const url = new URL(resultString)
+      content = url.pathname
 
       if (['nano:', 'nanorep:', 'xrb:'].includes(url.protocol) && this.util.account.isValidAccount(url.pathname)) {
-        type = 'account';
+        type = 'account'
       } else if (['nanoseed:', 'nanokey:'].includes(url.protocol) && this.util.nano.isValidHash(url.pathname)) {
-        type = 'hash';
+        type = 'hash'
       }
     } else {
-      type = 'generic';
-      content = resultString;
+      type = 'generic'
+      content = resultString
     }
 
     // check that the result is valid and matched the requested type
     if (type != null && type === this.type || this.type === 'generic') {
-      this.activeModal.close({reference: this.reference, content: content});
+      this.activeModal.close({ reference: this.reference, content: content })
     } else {
-      this.notifcationService.sendWarning('This QR code is not recognized.', { length: 5000, identifier: 'qr-not-recognized' });
-      return;
+      this.notifcationService.sendWarning('This QR code is not recognized.', { length: 5000, identifier: 'qr-not-recognized' })
+      return
     }
   }
 
-  onDeviceSelectChange(selected: string) {
-    const device = this.availableDevices.find(x => x.deviceId === selected);
-    this.currentDevice = device || null;
+  onDeviceSelectChange (selected: string) {
+    const device = this.availableDevices.find(x => x.deviceId === selected)
+    this.currentDevice = device || null
   }
 
-  onHasPermission(has: boolean) {
-    this.hasPermission = has;
+  onHasPermission (has: boolean) {
+    this.hasPermission = has
   }
 
-  onTorchCompatible(isCompatible: boolean): void {
-    this.torchAvailable$.next(isCompatible || false);
+  onTorchCompatible (isCompatible: boolean): void {
+    this.torchAvailable$.next(isCompatible || false)
   }
 
-  toggleTorch(): void {
-    this.torchEnabled = !this.torchEnabled;
+  toggleTorch (): void {
+    this.torchEnabled = !this.torchEnabled
   }
 
-  toggleTryHarder(): void {
-    this.tryHarder = !this.tryHarder;
+  toggleTryHarder (): void {
+    this.tryHarder = !this.tryHarder
   }
 }
